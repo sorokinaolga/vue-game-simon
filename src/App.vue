378f20b42b6096206851ecb47fdfbdbff8803988
sorokinaolga <sorokinaop@list.ru>
2021-12-31 00:35:00 +0300

    Инициализация проекта

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <GameBoard />
+  </div>
+</template>
+
+<script>
+import GameBoard from './components/game-board.vue';
+
+export default {
+  name: 'App',
+  components: {
+    GameBoard,
+  }
+}
+</script>
+
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  color: #333
+  text-align: center
+  max-width: 600px
+  margin: 0 auto
+</style>
---
+++ src/components/game-board.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="game-title">Simon Says</h1>
+</template>
+
+<script>
+export default {
+  name: 'Gameboard',
+}
+</script>
+
+<style lang="sass">
+.game-title
+  margin: 30px 0 60px
+</style>
