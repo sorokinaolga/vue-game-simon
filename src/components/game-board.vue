faa9ede41ea977d83b6ef508f59030ea3f5af987
sorokinaolga <sorokinaop@list.ru>
2021-12-31 09:48:35 +0300

    Верстка и стилизация проекта

--- src/components/game-board.vue
+++ src/components/game-board.vue
@@ -1,14 +1,121 @@
 <template>
-  <h1 class="game-title">Simon Says</h1>
+  <div>
+    <h1 class="game-title">Simon Says</h1>
+    <div class="container">
+      <div class="game-board">
+        <ul class="game-board__list">
+          <GameButton 
+            v-for="(button, index) in gameButtons"
+            :button="button"
+            :key="button"
+            :value="index"
+          />
+        </ul>
+      </div>
+      <div class="game-control">
+        <h2 class="game-control__title">Раунд: {{ round }}</h2>
+        <button class="btn">Старт</button>
+        <p>Извини, ты проиграл после {{ round }} раунда!</p>
+        <h2 class="game-control__title">Уровень сложности:</h2>
+        <ul class="game-control__list">
+          <li v-for="item in gameLevels" :key="item.value" class="game-control__level">
+            <input
+              type="radio"
+              :id="item.value"
+              name="level"
+              :value="item.value"
+              v-model="level"
+            >
+            <label :for="item.value">
+              {{ item.content }}
+            </label>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import GameButton from './game-button.vue';
+
 export default {
-  name: 'Gameboard',
+  name: 'GameBoard',
+  components: {
+    GameButton
+  },
+  data() {
+    return {
+      gameButtons: ["blue", "red", "yellow", "green"],
+      gameLevels: { 
+        easy: {
+          value: "easy",
+          content: "Легкий",
+          mc: 1500,
+        },
+        middle: {
+          value: "middle",
+          content: "Средний",
+          mc: 1000,
+        },
+        hard: {
+          value: "hard",
+          content: "Сложный",
+          mc: 400,
+        }
+      },
+      round: 0,
+      level: "easy",
+    }
+  }
 }
 </script>
 
 <style lang="sass">
 .game-title
   margin: 30px 0 60px
+
+.container
+  display: flex
+  flex-direction: row
+
+.game-board
+  background: #fff
+  position: relative
+  width: 302px
+  height: 295px
+  border-radius: 150px 150px 150px 150px
+  margin-right: 20px
+
+.game-board__list
+  list-style: none
+  margin: 0
+  padding: 0
+
+.game-control
+  text-align: left
+
+.game-control__title
+  margin: 20px 0
+
+.btn
+  width: 5em
+  box-sizing: border-box
+  font-size: 1.4em
+  border-radius: 10px 10px 10px 10px
+  background: #6dabe8
+  border: none
+  padding: 0.3em 0.6em
+
+.btn:hover
+  background: #78bcff
+
+.game-control__list
+  list-style: none
+  margin: 0
+  padding: 0
+
+.game-control__level
+  margin: 0
+  padding: 3px 0
 </style>
---
+++ src/components/game-button.vue
@@ -0,0 +1,59 @@
+<template>
+  <li 
+    :class="`game-board__item game-board__item--${button}`"
+    :value="value"
+  ></li>
+</template>
+
+<script>
+export default {
+  name: 'GameButton',
+  props: {
+    button: {
+      type: String,
+    },
+    value: {
+      type: Number,
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+.game-board__item
+  margin: 0
+  padding: 0
+  opacity: 0.6
+  transition: opacity 250ms ease
+  height: 290px
+  border-radius: 150px 150px 150px 150px
+  position: absolute
+  text-indent: 10000px
+  cursor: pointer
+
+.game-board__item:hover
+  border: 2px solid black
+
+.game-board__item.active
+  opacity: 1
+
+.game-board__item--red
+  background: #ff5643
+  clip: rect(0px, 300px, 150px, 150px)
+  width: 296px
+
+.game-board__item--blue
+  background: #1e90ff
+  clip: rect(0px, 150px, 150px, 0px)
+  width: 300px
+
+.game-board__item--yellow
+  background: #feef33
+  clip: rect(150px, 150px, 300px, 0px)
+  width: 300px
+
+.game-board__item--green
+  background: #bede15
+  clip: rect(150px,300px, 300px, 150px)
+  width: 296px
+</style>
